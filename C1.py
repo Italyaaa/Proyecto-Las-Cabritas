ca714f01436eab66d5cb9482ec6f28b13cc6f73d
EmilioRP8 <emilio.ruch@gmail.com>
2025-04-07 14:15:37 -0600

    P1

--- C1.py
+++ C1.py
@@ -6,3 +6,28 @@ def mostrar_menu():
     print("4. Opción 4: Agua mineral")
     print("5. Salir")
     print("===========================")
+
+
+def ejecutar_menu():
+    while True:
+        mostrar_menu()
+        try:
+            opcion = int(input("Selecciona una opción (1-5): "))
+            
+            if opcion == 1:
+                print("Has seleccionado 'Ver información'.")
+            elif opcion == 2:
+                print("Has seleccionado 'Agregar datos'.")
+            elif opcion == 3:
+                print("Has seleccionado 'Actualizar datos'.")
+            elif opcion == 4:
+                print("Has seleccionado 'Eliminar datos'.")
+            elif opcion == 5:
+                print("¡Hasta luego!")
+                break
+            else:
+                print("Opción inválida. Por favor, selecciona entre 1 y 5.")
+        except ValueError:
+            print("Entrada inválida. Por favor, ingresa un número.")
+        
+        print("\n")  # Espacio para mejor legibilidad entre iteraciones
