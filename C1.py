d9bbb16a1dad90efb2ce9ba098ecd8aee4e163a0
Italyaaa <a01277667@tec.mx>
2025-04-07 14:05:57 -0600

    uno

---
+++ C1.py
@@ -0,0 +1,8 @@
+def mostrar_menu():
+    print("===== Menú Principal =====")
+    print("1. Opción 1: Pasta")
+    print("2. Opción 2: Lasagna")
+    print("3. Opción 3: Limonada")
+    print("4. Opción 4: Agua mineral")
+    print("5. Salir")
+    print("===========================")
